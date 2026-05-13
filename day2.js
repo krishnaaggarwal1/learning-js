4fabec7acb27517179aa4794775fdf3c1c06239e
Krishna <krishna100aggarwal@gmail.com>
2026-05-13 07:16:17 +0530

    node day2.js

---
+++ day2.js
@@ -0,0 +1,6 @@
+let name ='krishna';
+const age=26;
+let isMarried=false;
+var city ='bangalore';
+console.log(name,age,isMarried,city);
+console.table([typeof name, typeof age, typeof isMarried, typeof city]);
